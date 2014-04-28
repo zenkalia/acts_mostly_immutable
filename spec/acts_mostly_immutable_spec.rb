952dde5a496aa2370c97ed70d851a3f5efe4c1a0
Mike Kendall <zenkalia@gmail.com>
2014-04-27 19:35:47 -0700

    :bento: shared contexts are cool

--- spec/acts_mostly_immutable_spec.rb
+++ spec/acts_mostly_immutable_spec.rb
@@ -2,6 +2,18 @@ require 'spec_helper'
 require 'acts_mostly_immutable'
 require 'support/active_record'
 
+
+shared_context 'whatever_class' do
+  before do
+    class Whatever < ActiveRecord::Base
+      include ActsMostlyImmutable
+    end
+  end
+  after do
+    Object.send(:remove_const, :Whatever)
+  end
+end
+
 describe 'ActsMostlyImmutable' do
   describe 'functions have a configurable prefix' do
     before do
@@ -19,11 +31,7 @@ describe 'ActsMostlyImmutable' do
     end
   end
   describe 'instance methods' do
-    before do
-      class Whatever < ActiveRecord::Base
-        include ActsMostlyImmutable
-      end
-    end
+    include_context 'whatever_class'
     it 'can be mixed in?' do
       Whatever.new.party.should == 'time'
     end
